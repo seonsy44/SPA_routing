03e4ecf3f30d9786d42da936c3199fe624826088
seon <seonsy44@gmail.com>
2022-11-29 22:52:14 +0900

    feat: Route

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import Route from './router/Route';
+import Router from './router/Router';
+import Routes from './router/Routes';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-router/src/router/Route.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  path: string;
+  element: JSX.Element;
+};
+
+function Route({ path, element }: Props) {
+  return element;
+}
+
+export default Route;
